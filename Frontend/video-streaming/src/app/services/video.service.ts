5bc54196f80104e9f011f071825204865c2e3fc9
Rohan Gowda <rohan.gowda@gmail.com>
2024-04-24 13:47:28 +0530

    1.changed the base url format

--- Frontend/video-streaming/src/app/services/video.service.ts
+++ Frontend/video-streaming/src/app/services/video.service.ts
@@ -1,21 +1,32 @@
+import { VideoReaction } from './../models/videoreaction';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Video } from '../models/Video';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class VideoService {
+  disLikeVideo(videoId:number): Observable<VideoReaction> {
+    return this.http.post<VideoReaction>(environment.baseUrl + "/video/disLikeVideo/"+videoId,{});
+  }
+  likeVideo(videoId:number):Observable<VideoReaction> {
+    return this.http.post<VideoReaction>(environment.baseUrl + "/video/likeVideo/"+videoId,{});
+  }
+  getVideoReaction(videoId:number): Observable<VideoReaction>{
+  return this.http.get<VideoReaction>(environment.baseUrl + "/video/getVideoReactionCount/"+videoId);
+  }
   saveDetails(metaData:Video):Observable<Video> {
-    return this.http.put<Video>("http://localhost:8080/vsa/video/videoDetails",metaData);
+    return this.http.put<Video>(environment.baseUrl + "/video/videoDetails",metaData);
   }
   uploadThumbnail(file :File,videoId:number):Observable<any> {
     const formData = new FormData();
     formData.append("file",file);
     formData.append("videoId",videoId.toString());
-    return this.http.post("http://localhost:8080/vsa/video/uploadThumbnail",formData,{responseType:'text'});
+    return this.http.post(environment.baseUrl + "/video/uploadThumbnail",formData,{responseType:'text'});
   }
 
   constructor(private http:HttpClient) { }
@@ -23,14 +34,14 @@ export class VideoService {
   uploadVideo(file:File):Observable<Video>{
     const formData = new FormData();
     formData.append("file",file)
-    return this.http.post<Video>("http://localhost:8080/vsa/video/uploadVideo",formData);
+    return this.http.post<Video>(environment.baseUrl + "/video/uploadVideo",formData);
   }
 
   getVideo(videoId:number):Observable<Video>{
-    return this.http.get<Video>("http://localhost:8080/vsa/video/getVideo/"+videoId);
+    return this.http.get<Video>(environment.baseUrl + "/video/getVideo/"+videoId);
   }
 
   getAllVideos():Observable<Array<Video>>{
-    return this.http.get<Array<Video>>("http://localhost:8080/vsa/video/getAllVideos");
+    return this.http.get<Array<Video>>(environment.baseUrl + "/video/getAllVideos");
   }
 }
